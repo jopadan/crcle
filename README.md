1bc2557f1d7d4a78021b5fe283873b2f31da6131
Jon Daniel <jopadan@mailfence.com>
2024-09-18 18:48:54 +0200

    update examples

--- README.md
+++ README.md
@@ -32,7 +32,11 @@ namespace crc32
 
 int main(int argc, char** argv)
 {
-    printf("[CHK] .8X\n", crc32::mpeg2::compute("123456789", 9));
+	printf("[CHK] 0x%.8X CRC32_MPEG2 \"123456789\"\n", crc32::mpeg2::compute((const uint8_t*)"123456789", 9));
+	printf("[CHK] 0x%.8X CRC32_MPEG2 \"123456789\"\n", crc32::mpeg2::check());
+	printf("[CHK] 0x%.8X CRC32_CCITT \"123456789\"\n", crc32::ccitt::compute((const uint8_t*)"123456789", 9));
+	printf("[CHK] 0x%.8X CRC32_CCITT \"123456789\"\n", crc32::ccitt::check());
+	exit(EXIT_SUCCESS);
 }
 ```
 
--- examples/example.cpp
+++ examples/example.cpp
@@ -1,10 +1,12 @@
-#include <cstring>
-#include <filesystem>
+#include <cstdlib>
+#include <cstdio>
 #include <crcle/crcle.hpp>
 
 int main(int argc, char** argv)
 {
-	printf("CRC32_MPEG2: \"123456789\" check: 0x%.8X\n", crc32::mpeg2::check());
-	printf("CRC32_CCITT: \"123456789\" check: 0x%.8X\n", crc32::ccitt::check());
+	printf("[CHK] 0x%.8X CRC32_MPEG2 \"123456789\"\n", crc32::mpeg2::compute((const uint8_t*)"123456789", 9));
+	printf("[CHK] 0x%.8X CRC32_MPEG2 \"123456789\"\n", crc32::mpeg2::check());
+	printf("[CHK] 0x%.8X CRC32_CCITT \"123456789\"\n", crc32::ccitt::compute((const uint8_t*)"123456789", 9));
+	printf("[CHK] 0x%.8X CRC32_CCITT \"123456789\"\n", crc32::ccitt::check());
 	exit(EXIT_SUCCESS);
 }
